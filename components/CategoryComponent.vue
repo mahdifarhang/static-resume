8143fb2a7325d35e790c806c83622d120586d89d
Mahdi <mafarhang76@gmail.com>
2022-05-15 00:43:31 +0430

    raw skill and category components

---
+++ components/CategoryComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'CategoryComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/SkillComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'SkillComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
