fa97cd78e91ccbc15ae55f18e01a4059416348b3
Mahdi <mafarhang76@gmail.com>
2022-05-14 21:16:06 +0430

    add contacts section

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,40 @@
           <span class="resume-title">{{ capitalize(getName()) }}</span>
         </v-card-title>
         <v-card-subtitle>{{ capitalize(position) }}</v-card-subtitle>
+        <v-card-text>
+          <v-row>
+            <v-col cols="4">
+              <span class="section-title">{{ capitalize("Contacts") }}</span>
+              <v-row align="center">
+                <v-btn icon :href="`tel:${contacts.email}`">
+                  <v-icon>mdi-phone</v-icon>
+                </v-btn>
+                {{ contacts.phone }}
+              </v-row>
+              <v-row align="center">
+                <v-btn icon :href="`mailto:${contacts.email}`">
+                  <v-icon>mdi-email</v-icon>
+                </v-btn>
+                {{ contacts.email }}
+              </v-row>
+              <v-row>
+                <v-btn
+                  v-for="(value, key) in contacts.links"
+                  :key="key"
+                  icon
+                  :href="value"
+                >
+                  <v-icon>
+                    {{ icons[key] }}
+                  </v-icon>
+                </v-btn>
+              </v-row>
+            </v-col>
+            <v-col cols="8">
+              hello
+            </v-col>
+          </v-row>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -15,8 +49,20 @@
 import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'IndexPage',
+  data: () => {
+    return {
+      icons: {
+        telegram: 'mdi-telegram',
+        instagram: 'mdi-instagram',
+        gitlab: 'mdi-gitlab',
+        github: 'mdi-github',
+        twitter: 'mdi-twitter',
+        linkedIn: 'mdi-linkedin'
+      }
+    }
+  },
   computed: {
-    ...mapState('resume', ['position']),
+    ...mapState('resume', ['position', 'contacts']),
     ...mapGetters('resume', ['getName', 'capitalize'])
   }
 }
@@ -25,7 +71,13 @@ export default {
 <style>
 .resume-title {
   font-weight: 800;
-  font-size: 2em;
+  font-size: 2.5em;
   letter-spacing: 0.2em;
+}
+
+.section-title {
+  font-weight: 500;
+  font-size: 1.5em;
+  letter-spacing: 0.05em;
 }
 </style>
--- store/resume/state.js
+++ store/resume/state.js
@@ -2,6 +2,17 @@ export default () => {
   return {
     firstname: 'Mahdi',
     lastname: 'Farhang',
-    position: 'Fullstack Developer'
+    position: 'Fullstack Developer',
+    contacts: {
+      phone: '+989127234910',
+      email: 'mafarhang76@gmail.com',
+      links: {
+        linkedIn: 'https://www.linkedin.com/in/mahdi-farhang-bb3ab3202',
+        telegram: 'https://t.me/lVIahdi',
+        github: 'https://github.com/mahdifarhang',
+        gitlab: 'https://gitlab.com/mahdifarhang',
+        twitter: 'https://twitter.com/The_Mahdi_'
+      }
+    }
   }
 }
