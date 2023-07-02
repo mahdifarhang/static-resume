3f526703ed316a8ddbc3dd8e4940d7bc596e5e4f
Mahdi Farhang <mafarhang76@gmail.com>
2023-07-02 17:28:44 +0330

    ESLint fucks a little bit less

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,19 @@ module.exports = {
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // 0 - 'off'
+    // 1 - 'warn'
+    // 2 - 'error'
+    'padding-line-between-statements': [
+      2,
+      { blankLine: 'always', next: 'return', prev: '*' },
+      // Bellow line got commented from extended rules, NOT TO GET ERRORS FOR NOT HAVING BLANK LINE AFTER VARIABLE DECLARATIONS
+      // { blankLine: 'always', next: '*', prev: ['const', 'let'] },
+      { blankLine: 'any', next: ['const', 'let'], prev: ['const', 'let'] }
+    ]
+  }
+
 }
