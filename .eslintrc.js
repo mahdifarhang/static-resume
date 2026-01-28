4895ccbe5ab3bed9cfdc6df9efe8a52fa9ca65d8
Mahdi Nobitex <mafarhang76@gmail.com>
2026-01-28 15:24:09 +0330

    Preperation of project better.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     // 1 - 'warn'
     // 2 - 'error'
     'padding-line-between-statements': [
-      2,
+      1,
       { blankLine: 'always', next: 'return', prev: '*' },
       // Bellow line got commented from extended rules, NOT TO GET ERRORS FOR NOT HAVING BLANK LINE AFTER VARIABLE DECLARATIONS
       // { blankLine: 'always', next: '*', prev: ['const', 'let'] },
